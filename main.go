82c658c5cc10204dec664dfb88079117d3f3ca39
Bhiradit <bhiradit.d@gmail.com>
2024-02-03 15:31:02 +0700

    adding models

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
 
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvents)
+
 	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	// context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvents(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
